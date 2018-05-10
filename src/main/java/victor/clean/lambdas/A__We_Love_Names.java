264535ac5aceae44044963cf4a7ad9ba551aecc5
Victor Rentea <victorrentea@gmail.com>
2018-05-10 19:41:05 +0300

    minor adjust

--- src/main/java/victor/clean/lambdas/A__We_Love_Names.java
+++ src/main/java/victor/clean/lambdas/A__We_Love_Names.java
@@ -11,19 +11,6 @@ import lombok.Data;
 // get the list of users to UI
 
 
-class UserFacade {
-	
-	private UserRepo userRepo;
-	
-	public List<UserDto> getAllUsers() {
-		return userRepo.findAll().stream()
-				.map(UserDto::new)
-				.collect(toList());
-	}
-
-}
-
-
 
 
 
@@ -46,11 +33,6 @@ class User {
 
 @Data
 class UserDto {
-	public UserDto(User user) {
-		this.setUsername(user.getUsername());
-		this.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
-		this.setActive(user.getDeactivationDate() == null);
-	}
 	private String fullName;
 	private String username;
 	private boolean active;
--- src/main/java/victor/clean/lambdas/D2__Loan_Pattern.java
+++ src/main/java/victor/clean/lambdas/D2__Loan_Pattern.java
@@ -15,45 +15,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // export all orders to a file
 
 
-interface OrderRepo extends JpaRepository<Order, Long> { // Spring Data FanClub
-	Stream<Order> findByActiveTrue(); // 1 Mln orders ;)
-}
-abstract class BaseExporter {
-	private final static Logger log = LoggerFactory.getLogger(BaseExporter.class);
-			
-	public File exportFile(String fileName) throws Exception {
-		File file = new File("export/" + fileName);
-		try (Writer writer = new FileWriter(file)) {
-			writeContent(writer);
-			return file;
-		} catch (Exception e) {
-			// TODO send email notification
-			log.debug("Gotcha!", e); // TERROR-Driven Programming
-			throw e;
-		}
-	}
-	protected abstract void writeContent(Writer writer) throws IOException;
-}
-
-
-class OrderExporter extends BaseExporter {
-	private OrderRepo repo;
-	protected void writeContent(Writer writer) throws IOException {
-		writer.write("OrderID;Date\n");
-		repo.findByActiveTrue()
-		.map(o -> o.getId() + ";" + o.getCreationDate())
-		.forEach(Unchecked.consumer(writer::write));
-	}
-	
-}
-class UserExporter extends BaseExporter {
-//	private UserRepo repo;
-	protected void writeContent(Writer writer) throws IOException {
-		writer.write("UserID;FirstName...\n");
-//		repo.findByActiveTrue()
-//			.map(o -> o.getId() + ";" + o.getCreationDate())
-//			.forEach(Unchecked.consumer(writer::write));
-		
-	}
-}
 
