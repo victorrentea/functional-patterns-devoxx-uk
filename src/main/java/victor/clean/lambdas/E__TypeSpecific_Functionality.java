e05bbdb985f780ab670198fbaa78196d40a2d44d
Victor Rentea <victorrentea@gmail.com>
2018-05-11 14:44:39 +0300

    old school switch

--- src/main/java/victor/clean/lambdas/E__TypeSpecific_Functionality.java
+++ src/main/java/victor/clean/lambdas/E__TypeSpecific_Functionality.java
@@ -3,29 +3,35 @@ package victor.clean.lambdas;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+
 class Movie {
 	enum Type {
 		REGULAR, NEW_RELEASE, CHILDREN
 	}
-
+	
 	private final Type type;
-
+	
+	
 	public Movie(Type type) {
 		this.type = type;
 	}
 
+
 	public int computePrice(int days) {
 		switch (type) {
-		case REGULAR:
-			return days + 1;
-		case NEW_RELEASE:
-			return days * 2;
-		case CHILDREN:
-			return 5;
+		case REGULAR: return days + 1;
+		case NEW_RELEASE: return days * 2;
+		case CHILDREN: return 5;
+		default: throw new IllegalArgumentException();
 		}
-		return 0; // ?!.. Free!! Deducted from your salary!
 	}
 }
+
+
+
+
+
+
 
 public class E__TypeSpecific_Functionality {
 	public static void main(String[] args) {
