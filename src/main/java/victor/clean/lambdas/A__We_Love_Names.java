2339125f680d5c97d541ddcccb6e6481b76e9880
Victor Rentea <victorrentea@gmail.com>
2018-05-11 14:19:38 +0300

    mapping in the constructor

--- src/main/java/victor/clean/lambdas/A__We_Love_Names.java
+++ src/main/java/victor/clean/lambdas/A__We_Love_Names.java
@@ -10,21 +10,21 @@ import lombok.Data;
 
 // get the list of users to UI
 
+
 class UserFacade {
 	
 	private UserRepo userRepo;
 	
 	public List<UserDto> getAllUsers() {
-		List<User> users = userRepo.findAll();
-		List<UserDto> dtos = new ArrayList<>();
-		for (User user : users) {
-			UserDto dto = new UserDto();
-			dto.setUsername(user.getUsername());
-			dto.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
-			dto.setActive(user.getDeactivationDate() == null);
-			dtos.add(dto);
-		}
-		return dtos;
+		return userRepo.findAll().stream().map(this::toDto).collect(toList());
+	}
+
+	private UserDto toDto(User user) {
+		UserDto dto = new UserDto(user);
+//		dto.setUsername(user.getUsername());
+//		dto.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
+//		dto.setActive(user.getDeactivationDate() == null);
+		return dto;
 	}
 }
 
@@ -52,6 +52,11 @@ class User {
 
 @Data
 class UserDto {
+	public UserDto(User user) {
+		this.setUsername(user.getUsername());
+		this.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
+		this.setActive(user.getDeactivationDate() == null);
+	}
 	private String fullName;
 	private String username;
 	private boolean active;
